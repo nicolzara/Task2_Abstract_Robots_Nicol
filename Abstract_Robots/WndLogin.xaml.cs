8f4532cb9ff1b9c87a08d659af074cb0d9611db9
nicol zarankin <nicolzarankin@gmail.com>
2023-09-18 14:42:51 +0300

    Finished the third part

--- Abstract_Robots/WndLogin.xaml.cs
+++ Abstract_Robots/WndLogin.xaml.cs
@@ -8,10 +8,12 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace Robots_inc
 {
@@ -20,13 +22,16 @@ namespace Robots_inc
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<Worker> workers; //אוסף עובדים 
-        List<Mission> activeMissions;//אוסף משימות פעילות
-        List<RobotSpy> activeRobots; //אוסף רובוטים פעילים
+        List<Worker> _workers; //אוסף עובדים 
+        List<Mission> _activeMissions;//אוסף משימות פעילות
+        List<RobotSpy> _activeRobots; //אוסף רובוטים פעילים
 
         public MainWindow()
         {
             InitializeComponent();
+            _workers = CreateWorkers();
+            _activeRobots = CreateRobots();
+            _activeMissions = CreateMissions();
         }
 
         private void btnClose_Click(object sender, RoutedEventArgs e)
@@ -41,13 +46,122 @@ namespace Robots_inc
         //מנכ"ל אחד, 2 מנהלי תפעול ו-3 עובדי תפעול
         //כתבו זימון לפעולה שכתבתם בפעולה הבונה של החלון
 
+        public List<Worker> CreateWorkers()
+        {
+            List<Worker> workers = new List<Worker>();
+
+            Worker worker = new OperationalWorker("Opp", "123456", new DateTime(), "123456", 32, 150);
+            workers.Add(worker);
+            worker = new OperationalWorker("Bob", "654321", new DateTime(), "654321", 34, 145);
+            workers.Add(worker);
+            worker = new OperationalWorker("Alice", "135790", new DateTime(), "135790", 35.5, 155);
+            workers.Add(worker);
+
+            worker = new OperationManager("Marcel", "121212", new DateTime(), "121212", 45, 150, 0);
+            workers.Add(worker);
+            worker = new OperationManager("David", "131313", new DateTime(), "131313", 50, 145, 0);
+            workers.Add(worker);
+
+            worker = new GeneralManager("Carlos", "456789", new DateTime(), "456789", 10550, 50);
+            workers.Add(worker);
+
+            return workers;
+
+        }
+
+        public List<Worker> Workers()
+        {
+            List<Worker> workersToReturn = new List<Worker>();
+            int counterWorkers = 0;
+            int counterOperationalManager = 0;
+            int counterGeneralManager = 0;
+            foreach(Worker worker in this._workers)
+            {
+                if (workersToReturn.Count != 6)
+                {
+                    if(worker is OperationalWorker)
+                    {
+                        if (counterWorkers < 3)
+                        {
+                            workersToReturn.Add(worker);
+                            counterWorkers++;
+                        }
+                    }
+
+                    if(worker is GeneralManager)
+                    {
+                        if(counterGeneralManager == 0)
+                        {
+                            workersToReturn.Add(worker);
+                            counterGeneralManager++;
+                        }
+                    }
+
+                    if(worker is OperationalWorker)
+                    {
+                        if (counterOperationalManager < 2)
+                        {
+                            workersToReturn.Add(worker);
+                            counterOperationalManager++;
+                        }
+                    }               
+                }                
+            }
+
+            return workersToReturn;
+        }
+
         //משימה 2
         //כתבו פעולה המחזירה אוסף של 8 רובוטים
         //כתבו זימון לפעולה שכתבתם בפעולה הבונה של החלון
+        public List<RobotSpy> CreateRobots()
+        {
+            List<RobotSpy> robots = new List<RobotSpy>();
+
+            RobotSpy robot = new RobotFly(new DateTime(), 100);
+            robots.Add(robot);
+            robot = new RobotFly(new DateTime(), 50);
+            robots.Add(robot);
+            robot = new RobotFly(new DateTime(), 75);
+            robots.Add(robot);
+
+            robot = new RobotQuad(new DateTime(), 100);
+            robots.Add(robot);
+            robot = new RobotQuad(new DateTime(), 50);
+            robots.Add(robot);
+            robot = new RobotQuad(new DateTime(), 75);
+            robots.Add(robot);
+
+            robot = new RobotWheels(new DateTime(), 100);
+            robots.Add(robot);
+            robot = new RobotWheels(new DateTime(), 50);
+            robots.Add(robot);
+
+            return robots;
+        }
+
 
         //משימה 3
         //כתבו פעולה המחזירה אוסף של 5 משימות
-        //כתבו זימון לפעולה שכתבתם בפעולה הבונה של החלון       
+        //כתבו זימון לפעולה שכתבתם בפעולה הבונה של החלון
+        public List<Mission> CreateMissions()
+        {
+            List<Mission> missions = new List<Mission>();
+
+            Mission mission = new Mission(new DateTime(2024, 05, 09, 09, 15, 00), "put the box in the garage");
+            missions.Add(mission);
+            mission = new Mission(new DateTime(2024, 05, 06, 02, 15, 00), "find the best computer");
+            missions.Add(mission);
+            mission = new Mission(new DateTime(2023, 10, 08, 14, 15, 00), "take down the president");
+            missions.Add(mission);
+            mission = new Mission(new DateTime(2024, 03, 09, 08, 15, 00), "write a song about poop");
+            missions.Add(mission);
+            mission = new Mission(new DateTime(2024, 03, 04, 12, 15, 00), "take the dog out");
+            missions.Add(mission);
+
+            return missions;
+        }
+
 
         //משימה 4
         //login כתבו פעולה המגיבה לללחיצה על כפתור 
@@ -56,10 +170,19 @@ namespace Robots_inc
         private void btnLogin_Click(object sender, RoutedEventArgs e)
         {
             //...אם מספר הזיהוי והסיסמה תואמים לעובד ברשימה, אז
-            Worker worker = workers[?];
-            WndMain main = new WndMain(worker, activeMissions, activeRobots, workers);
-            main.ShowDialog();
+            string pass = tbxPassword.Password.ToString();
+            string id = tbxID.Text.ToString();
 
+            foreach (Worker worker in _workers)
+            {
+                if(worker.GetIdNumber().Equals(id) && worker.GetPassword().Equals(pass))
+                {
+                    WndMain main = new WndMain(worker, _activeMissions, _activeRobots, _workers);
+                    main.ShowDialog();
+                }
+            }
+
+            MessageBox.Show("Error in entering");
         }
     }
 }
